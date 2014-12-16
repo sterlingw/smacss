610fbfa572c52f3f95e3b1eaf10e67640aaf23d3
sterlingw <sterlingsrq@gmail.com>
2014-12-16 18:01:47 -0500

    fixed npm

--- index.js
+++ index.js
@@ -1,51 +1,40 @@
-module.exports = function() {
-  var fs = require('fs');
-
-  // Create folders
-  // ========================
+var smacss = require('./smacss.js');
+var fs = require('fs');
 
 
-  // Create folder and _index.scss file
-  function create(directory, filepath) {
-    var comment = '// Imports of ' + ' ' + directory + '...';
 
-    fs.mkdir(directory);
-
-    fs.writeFile(filepath, comment, function (err) {
-      console.log('Created ' + 'directory' + '...');
-    });
-  }
+// Create folders
+// ========================
 
 
-  // Base
-  create('base', 'base/_index.scss');
+// Base
+smacss.create('base', 'base/_index.scss');
 
-  // Layout
-  create('layout', 'layout/_index.scss');
+// Layout
+smacss.create('layout', 'layout/_index.scss');
 
-  // Modules
-  create('modules', 'modules/_index.scss');
+// Modules
+smacss.create('modules', 'modules/_index.scss');
 
-  // Themes
-  create('themes', 'themes/_index.scss');
+// Themes
+smacss.create('themes', 'themes/_index.scss');
 
-  // States
-  create('states', 'states/_index.scss');
+// States
+smacss.create('states', 'states/_index.scss');
 
-  // Themes
-  create('themes', 'themes/_index.scss');
+// Themes
+smacss.create('themes', 'themes/_index.scss');
 
-  // Utilities
-  create('utilities', 'utilities/_index.scss');
+// Utilities
+smacss.create('utilities', 'utilities/_index.scss');
 
-  // Utilites/Lib
-  create('utilities/lib/', 'utilities/lib/_index.scss');
+// Utilites/Lib
+smacss.create('utilities/lib/', 'utilities/lib/_index.scss');
 
-  // App.scss
-  fs.writeFile('app.scss', '// Imports of each directory', function (err) {
-    console.log('Created app.scss...');
-  });
+// App.scss
+fs.writeFile('app.scss', '// Imports of each directory', function (err) {
+  console.log('Created app.scss...');
+});
 
 
-  console.log('Scaffolding...');
-};
+console.log('Scaffolding...');
---
+++ smacss.js
@@ -0,0 +1,13 @@
+
+
+    // Create folder and _index.scss file
+    exports.create = function(directory, filepath) {
+        var comment = '// Imports of ' + ' ' + directory + '...';
+
+        fs.mkdir(directory);
+
+        fs.writeFile(filepath, comment, function (err) {
+            console.log('Created ' + 'directory' + '...');
+        });
+    };
+
