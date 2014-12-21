64279f64b20332d9f4b781b98b1d7d69663a1b33
sterlingw <sterlingsrq@gmail.com>
2014-12-21 16:38:40 -0500

    updated commenting.

--- smacss.js
+++ smacss.js
@@ -1,13 +1,19 @@
 var fs = require('fs');
 
-    // Create folder and _index.scss file
-    exports.create = function(directory, filepath) {
-        var comment = '// Imports of ' + ' ' + directory + '...';
 
-        fs.mkdir(directory);
+/**
+ * Creates a folder and _index.scss file for a SMACSS structure using filesystem.
+ *
+ * @param {string} directory - name of the directory you want to create e.g. modules
+ * @param {string} filepath - filepath for _index.scss
+ */
+exports.create = function(directory, filepath) {
+    var comment = '// Imports of ' + ' ' + directory + '...';
 
-        fs.writeFile(filepath, comment, function (err) {
-            console.log('Created ' + 'directory' + '...');
-        });
-    };
+    fs.mkdir(directory);
+
+    fs.writeFile(filepath, comment, function (err) {
+        console.log('Created ' + 'directory' + '...');
+    });
+};
 
